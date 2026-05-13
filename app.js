95adf88bbe22c79e85b7ebfaf52d3179aaa99c38
XavierTheSigma <xavierseth27.1@gmail.com>
2026-05-13 21:30:37 +0800

    Fix typo in supabase client initialization

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // supabase config stuff
 const supabase_url = 'https://ovjsgpfasqhaiuseqdcv.supabase.co'
 const supabase_key = 'sb_publishable_G-xVWk4BgqinikDuOJ0Piw_oVZNNdYL'
-const sb = supabase_key.createClient(supabase_url, supabase_key)
+const sb = supabase.createClient(supabase_url, supabase_key)
 
 // sates
 let currentSort = 'newest'
